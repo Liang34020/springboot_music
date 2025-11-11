c91c4798220fc0fc74f79736be008a5481030957
Liang34020 <anthonychao701@gmail.com>
2025-11-11 11:05:33 +0800

    最愛清單 - 檢查 user 是否存在

--- src/main/java/com/anthony/springboot_music/service/impl/OrderServiceImpl.java
+++ src/main/java/com/anthony/springboot_music/service/impl/OrderServiceImpl.java
@@ -3,15 +3,21 @@ package com.anthony.springboot_music.service.impl;
 
 import com.anthony.springboot_music.dao.MusicDao;
 import com.anthony.springboot_music.dao.OrderDao;
+import com.anthony.springboot_music.dao.UserDao;
 import com.anthony.springboot_music.dto.FavoriteItem;
 import com.anthony.springboot_music.dto.CreateOrderRequest;
 import com.anthony.springboot_music.model.Music;
 import com.anthony.springboot_music.model.Order;
 import com.anthony.springboot_music.model.OrderItem;
+import com.anthony.springboot_music.model.User;
 import com.anthony.springboot_music.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -21,11 +27,17 @@ import java.util.List;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
+
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private MusicDao musicDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -41,6 +53,13 @@ public class OrderServiceImpl implements OrderService {
     @Transactional //只要有作多個DAO層的呼叫 都要做這個處理 確保兩個呼叫都是成功才寫入
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+//      檢查user是否存在
+        User user = userDao.getUserById(userId);
+
+        if (user == null) {
+            log.warn("該 userId={} 不存在", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
 //      設定時間格式
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
